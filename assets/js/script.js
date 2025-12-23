24374ebffef19d707a77ede7108e1b43c0de28c6
gamergaga <gamergaga786@gmail.com>
2025-12-23 17:24:23 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,30 @@
-jQuery(document).ready(function ($) {
+jQuery(function ($) {
     'use strict';
 
-    const $dropzone = $('.cacp-dropzone');
-    const $fileInput = $('#cacp-file-input');
-    const $preview = $('.cacp-preview');
-    const $img = $('#cacp-preview-img');
+    const $editBtn  = $('.cacp-edit-avatar-btn');
+    const $dropdown = $('.cacp-avatar-dropdown');
+    const $file     = $('#cacp-file-input');
+    const $action   = $('#cacp_avatar_action');
 
-    // Ensure form is ready for files
-    $('#commentform').attr('enctype', 'multipart/form-data');
-
-    // Add Icon
-    const icon = '<svg class="cacp-icon" viewBox="0 0 24 24"><path d="M19.35 10.04C18.67 6.59 15.64 4 12 4 9.11 4 6.6 5.64 5.35 8.04 2.34 8.36 0 10.91 0 14c0 3.31 2.69 6 6 6h13c2.76 0 5-2.24 5-5 0-2.64-2.05-4.78-4.65-4.96zM14 13v4h-4v-4H7l5-5 5 5h-3z"/></svg>';
-    $dropzone.prepend(icon);
-
-    // CLICK FIX: Trigger the hidden file input when clicking the dropzone
-    $dropzone.on('click', function(e) {
+    $editBtn.on('click', function (e) {
         e.preventDefault();
-        $fileInput.trigger('click');
+        $dropdown.toggle();
     });
 
-    // File selection logic
-    $fileInput.on('change', function(e) {
-        const file = e.target.files[0];
-        if (file) {
-            const reader = new FileReader();
-            reader.onload = (e) => {
-                $img.attr('src', e.target.result);
-                $dropzone.hide();
-                $preview.show().css('display', 'flex');
-            };
-            reader.readAsDataURL(file);
+    $('.cacp-avatar-change').on('click', function () {
+        $action.val('change');
+        $dropdown.hide();
+        $file.trigger('click');
+    });
+
+    $('.cacp-avatar-remove').on('click', function () {
+        $action.val('remove');
+        $dropdown.hide();
+    });
+
+    $(document).on('click', function (e) {
+        if (!$(e.target).closest('.cacp-edit-avatar-btn, .cacp-avatar-dropdown').length) {
+            $dropdown.hide();
         }
-    });
-
-    // Remove selection
-    $('.cacp-remove').on('click', function(e) {
-        e.preventDefault();
-        $fileInput.val('');
-        $preview.hide();
-        $dropzone.show();
     });
 });
